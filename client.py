44739358a9963c9850d0b4b69ef4301241c3f931
chensitongc <chensitongc@gmail.com>
2024-03-03 00:00:00 +0800

    Add function ('async ', 'send_message') from ./client.py on 2024-03-03

--- client.py
+++ client.py
@@ -377,3 +377,4 @@ if __name__ == '__main__':
 # Function: ('async ', 'join_room')
 # Function: ('async ', 'leave_room')
 # Function: ('async ', 'start_game')
+# Function: ('async ', 'send_message')
